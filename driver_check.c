e3ab6ff8e2536c581a091958a6c616d1a217d513
Yao-Hippolyte FOLLY <yao-hippolyte.folly@ens.uvsq.fr>
2024-03-18 14:37:22 +0100

    Modification de driver_calibre et driver_check

--- driver_check.c
+++ driver_check.c
@@ -5,19 +5,19 @@
 extern uint64_t rdtsc ();
 
 // TODO: adjust for each kernel
-extern void kernel (unsigned n, float a[n][n], float b[n][n], float c[n][n]);
+extern void kernel (unsigned n, double a[n][n], double b[n][n]);
 
 // TODO: adjust for each kernel
-static void init_array (int n, float a[n][n]) {
+static void init_array (int n, double a[n][n]) {
    int i, j;
 
    for (i=0; i<n; i++)
       for (j=0; j<n; j++)
-         a[i][j] = (float) rand() / RAND_MAX;
+         a[i][j] = (double) rand() / RAND_MAX;
 }
 
 // TODO: adjust for each kernel
-static void print_array (int n, float a[n][n], const char *output_file_name) {
+static void print_array (int n, double a[n][n], const char *output_file_name) {
    int i, j;
 
    FILE *fp = fopen (output_file_name, "w");
@@ -28,7 +28,7 @@ static void print_array (int n, float a[n][n], const char *output_file_name) {
 
    for (i=0; i<n; i++)
       for (j=0; j<n; j++)
-         fprintf (fp, "%f\n", a[i][j]);
+         fprintf (fp, "%lf\n", a[i][j]);
 
    fclose (fp);
 }
@@ -45,9 +45,8 @@ int main (int argc, char *argv[]) {
    const char *output_file_name = argv[2];
 
    /* allocate arrays. TODO: adjust for each kernel */
-   float (*a)[size] = malloc (size * size * sizeof a[0][0]);
-   float (*b)[size] = malloc (size * size * sizeof b[0][0]);
-   float (*c)[size] = malloc (size * size * sizeof c[0][0]);
+   double (*a)[size] = malloc (size * size * sizeof a[0][0]);
+   double (*b)[size] = malloc (size * size * sizeof b[0][0]);
 
    /* init arrays */
    srand(0);
@@ -55,13 +54,12 @@ int main (int argc, char *argv[]) {
    init_array (size, b);
 
    /* print output */
-   kernel (size, a, b, c);
-   print_array (size, c, output_file_name);
+   kernel (size, a, b);
+   print_array (size, b, output_file_name);
 
    /* free arrays. TODO: adjust for each kernel */
    free (a);
    free (b);
-   free (c);
-
+   
    return EXIT_SUCCESS;
 }
