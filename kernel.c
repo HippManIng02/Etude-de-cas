20499e7e09b39f802455e7b5b9506e669d03f6b1
Yao-Hippolyte FOLLY <yao-hippolyte.folly@ens.uvsq.fr>
2024-05-04 12:31:51 +0200

    correction

--- kernel.c
+++ kernel.c
@@ -3,9 +3,12 @@
 //Remove using of middle table
 #include <stdlib.h> // malloc , free
 #include <math.h>   // pow
+#include <stdio.h>
+
 void kernel(unsigned n, const double a[n][n],double b[n][n])
 
 {
+    printf("Je suis OPT1!\n");
     unsigned i, j;
     for (j = 0; j < n; j++)
     {
@@ -31,9 +34,12 @@ void kernel(unsigned n, const double a[n][n],double b[n][n])
 //Remove using the pow function
 #include <stdlib.h> // malloc , free
 #include <math.h>   // pow
+#include <stdio.h>
+
 void kernel(unsigned n, const double a[n][n],double b[n][n])
 
 {
+    printf("Je suis OPT2!\n");
     unsigned i, j;
     for (j = 0; j < n; j++)
     {
@@ -55,7 +61,78 @@ void kernel(unsigned n, const double a[n][n],double b[n][n])
     }
 }
 
+
+#elif defined OPT3
+/* Inversion de boocle */
+
+#include <stdlib.h> // malloc , free
+#include <math.h>   // pow
+#include <stdio.h>
+
+void kernel(unsigned n, const double a[n][n],double b[n][n])
+
+{
+    printf("Je suis OPT3!\n");
+    unsigned i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i < j)
+            {
+                c[i][j] = 0.0;
+            }
+            else if (i > j)
+            {
+                c[i][j] = 2.0;
+            }
+            else
+            {
+                c[i][j] = 1.0;
+            }
+        }
+        for (j = 0; j < n; j++)
+        {
+            b[i][j] = pow(a[i][j], c[i][j]);
+        }
+    }
+}
+
+#elif defined OPT4
+/* Inversion de boocle + OPT1 et OPT2*/
+
+#include <stdlib.h> // malloc , free
+#include <math.h>   // pow
+#include <stdio.h>
+
+void kernel(unsigned n, const double a[n][n],double b[n][n])
+
+{
+    printf("Je suis OPT4!\n");
+    unsigned i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i < j)
+            {
+                 b[i][j] = 1;
+            }
+            else if (i > j)
+            {
+                 b[i][j] = a[i][j] * a[i][j];
+            }
+            else
+            {
+                 b[i][j] = a[i][j];
+            }
+        }
+    }
+}
+
+
 #elif define OPT5
+/* Élimination des conditions */
 #include <stdlib.h> // malloc , free
 #include <math.h> // pow
 
@@ -73,20 +150,23 @@ void kernel(unsigned n, const double a[n][n],double b[n][n])
             b[i][j] = 2;
         }
 
-                 b[i][j] = 1;
-            }
+        b[i][i] = 1;
+    }
 }
 
 
-#else
 
+#else
 /* original */
 
 #include <stdlib.h> // malloc , free
 #include <math.h>   // pow
+#include <stdio.h>
+
 void kernel(unsigned n, const double a[n][n],double b[n][n])
 
 {
+    printf("Je suis le main!\n");
     unsigned i, j;
     double(*c)[n] = malloc(n * n * sizeof c[0][0]);
     for (j = 0; j < n; j++)
